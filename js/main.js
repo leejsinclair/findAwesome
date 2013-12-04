f112f95ca6dca5b4e0af21f8f1911eea2d63305c
Lee Sinclair <lee.j.sinclair@gmail.com>
2013-12-04 11:22:50 +1000

    script/style optomisation

--- js/main.js
+++ js/main.js
@@ -417,14 +417,6 @@ function app($scope) {
       {'name':'fa-youtube-play','searchText':'fa-youtube-play youtube-play social video video audio'},
       {'name':'fa-youtube-square','searchText':'fa-youtube-square youtube-square social'}
     ];
-}
 
-$(function() {
-  $("body").on("input propertychange", ".search", function(e) {
-    $(this).toggleClass("search-with-value", !!$(e.target).val());
-  }).on("focus", ".search", function() {
-    $(this).addClass("search-with-focus");
-  }).on("blur", ".search", function() {
-    $(this).removeClass("search-with-focus");
-  });
-});
+  $("#loading").css("display","none");
+}
